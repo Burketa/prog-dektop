b0617af4e88abfe9eb18c42ef2e4d4d3c895eae1
burca <rlpelizzon@hotmai.com>
2019-08-27 20:31:08 -0300

    adicionada leitura de arquivo com buffered reader

---
+++ projeto/src/semanaFile/ControleBuffered.java
@@ -0,0 +1,62 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2019 burca.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package semanaFile;
+
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author burca
+ */
+public class ControleBuffered {
+
+    private File arquivo = null;
+
+    public ControleBuffered(File arquivo) {
+        this.arquivo = arquivo;
+    }
+
+    public String lerArquivo() {
+        //String string = "";
+        StringBuilder string = new StringBuilder();
+        FileReader fileReader = null;
+        BufferedReader bufferedReader = null;
+
+        try {
+            fileReader = new FileReader(arquivo);
+            bufferedReader = new BufferedReader(fileReader);
+
+            while (bufferedReader.ready()) {
+                string.append(bufferedReader.readLine()).append("\n");
+            }
+        } catch (IOException exp) {
+            System.err.println(exp);
+        } finally {
+            return string.toString();
+        }
+    }
+
+}
--- projeto/src/semanaFile/TestaArquivoOtimizado.java
+++ projeto/src/semanaFile/TestaArquivoOtimizado.java
@@ -9,55 +9,56 @@ import java.util.Date;
 /**
  * @author burca
  */
-public class TestaArquivo {
+public class TestaArquivoOtimizado {
 
     public static void main(String[] args) throws IOException {
-        String pathString = "C:/Users/Aluno/Desktop/utfpr-desktop/projeto/escrita";
-
-        File diretorio = new File(pathString);
-        diretorio.mkdir();
-
-        //1 - ligacao da linguagem com o sistema de arquivos
+        //String pathString = "C:/Users/Aluno/Desktop/utfpr-desktop/projeto/escrita/log.txt";
+        File arquivo = new File("C:/Users/Aluno/Desktop/utfpr-desktop/projeto/escrita/log.txt");
+        ControleBuffered controle = new ControleBuffered(arquivo);
+        String conteudo = controle.lerArquivo();
+        System.out.println(conteudo);
+        /*//1 - ligacao da linguagem com o sistema de arquivos
         File arquivoLog = new File(diretorio, "log.txt");
         //2 - capacidade de escrita de texto em arquivo
         FileWriter escritor = new FileWriter(arquivoLog, false); //segundo parametro eh pra concatenar ou nao, default é false
         //3- capacidade de buffer (RAM)
         BufferedWriter escritorBuffer = new BufferedWriter(escritor);
-
+        
         File subdir = new File(diretorio, "subdir1");
         subdir.mkdir();
-
+        
         String[] arquivos = diretorio.list();
         System.out.println(arquivos);
-
+        
         assert arquivos != null;
-
+        
         escritorBuffer.write("Log criado em " + new Date(System.currentTimeMillis()));
         escritorBuffer.newLine();
         escritorBuffer.newLine();
         
         for (String path : arquivos) {
-            File filho = new File(diretorio, path);
-
-            escritorBuffer.write(filho.getAbsolutePath());
-            escritorBuffer.newLine();
-            escritorBuffer.write("" + filho.getParent());
-            escritorBuffer.newLine();
-            escritorBuffer.write("É diretorio? " + filho.isDirectory());
-            escritorBuffer.newLine();
-            escritorBuffer.write("É arquivo? " + filho.isFile());
-            escritorBuffer.newLine();
-            escritorBuffer.write("Tamanho = " + filho.length() + " bytes./n");
-            escritorBuffer.flush();
+        File filho = new File(diretorio, path);
+        
+        escritorBuffer.write(filho.getAbsolutePath());
+        escritorBuffer.newLine();
+        escritorBuffer.write("" + filho.getParent());
+        escritorBuffer.newLine();
+        escritorBuffer.write("É diretorio? " + filho.isDirectory());
+        escritorBuffer.newLine();
+        escritorBuffer.write("É arquivo? " + filho.isFile());
+        escritorBuffer.newLine();
+        escritorBuffer.write("Tamanho = " + filho.length() + " bytes./n");
+        escritorBuffer.flush();
         }
         escritorBuffer.newLine();
         escritorBuffer.write("Fim.");
         escritorBuffer.close();
-
+        
         /*if (arquivoLog.exists()) {
-            arquivoLog.delete();
+        arquivoLog.delete();
         } else {
-            arquivoLog.createNewFile();
+        arquivoLog.createNewFile();
         }*/
+        //}*/
     }
 }
