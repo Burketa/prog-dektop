7d07c6e2cd28f973edd5c41a87092a781a24e224
burca <rlpelizzon@gmail.com>
2019-11-05 20:32:46 -0300

    ñao deu certo, o erro no JPAMetaModelEntityProcessor.java

--- projeto/src/aulaHibernate/GeraTabelas.java
+++ projeto/src/aulaHibernate/GeraTabelas.java
@@ -1,9 +1,13 @@
 package aulaHibernate;
 
+import javax.annotation.processing.SupportedSourceVersion;
+import static javax.lang.model.SourceVersion.RELEASE_7;
 import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.tool.hbm2ddl.SchemaExport;
 
+ @SupportedSourceVersion(RELEASE_7)
 public class GeraTabelas {
+   
     public static void main(String[] args) {
         // Cria uma configuracao para a classe Produto
         AnnotationConfiguration cfg = 
